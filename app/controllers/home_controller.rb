6ac8e43cc8bbdb380e6c52bf3f942e36fa83c59a
Essi No <essi.norri@outlook.com>
2021-01-27 17:25:43 +0200

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  	#@about_me = "This website is based on a Codemy.com tutorial."
+  	#@answer = 2 + 2; #trying out stuff
+  end
+  
+end
